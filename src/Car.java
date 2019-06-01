7ee5a8fc1965dc704e8007e619d3f1934c9202d8
p-derezinski <p.derezinski@gmail.com>
2019-06-01 12:16:52 +0200

    Car Mathods

--- src/Car.java
+++ src/Car.java
@@ -2,21 +2,29 @@ import java.time.Year;
 
 public class Car {
 
-    private Year productionYear;
+    private String productionYear;
     private String model;
     private int speed;
 
-    public Car(Year productionYear, String model) {
+    public Car(String productionYear, String model) {
         this.productionYear = productionYear;
         this.model = model;
         this.speed = 0;
     }
 
-    public Year getProductionYear() {
+    public void accelerate() {
+        this.speed += 5;
+    }
+
+    public void slowDown() {
+        this.speed -= 5;
+    }
+
+    public String getProductionYear() {
         return productionYear;
     }
 
-    public void setProductionYear(Year productionYear) {
+    public void setProductionYear(String productionYear) {
         this.productionYear = productionYear;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.time.Year;
 import java.util.Scanner;
 
 public class Main {
@@ -14,5 +15,17 @@ public class Main {
         System.out.println("Podaj rok produkcji:");
         year = scanner.nextLine();
         System.out.println(model + " " + year);
+
+        Car car = new Car(year, model);
+        car.accelerate();
+        System.out.println("The current speed is " + car.getSpeed());
+        for (int i = 0; i < 5; i++) {
+            car.accelerate();
+        }
+        System.out.println("The current speed is " + car.getSpeed());
+        while (car.getSpeed() != 0) {
+            car.slowDown();
+        }
+        System.out.println("The current speed is " + car.getSpeed());
     }
 }
